7c3b7b4dad39711c6c491fa9bc5fda345a960d8d
Mirian de Freitas <106813546+Mirian007@users.noreply.github.com>
2024-04-29 08:43:01 -0300

    mainjs

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
-let loop1 = "pink";
+function verificarTime() {
 
+  var continuarPerguntando = true;
 
-while (loop1 !== "pink") {
-loop1 = prompt("A palavra pink em inglês é qual cor em português?");
+  while (continuarPerguntando) {
 
-  if (loop1 === "rosa") {
-    alert("Você é competente!");
-  } else {
-    alert(Você é incompetente);
+    var respostaTime = prompt("A palavra pink em inglês é qual cor em português?");
+
+    
+    if (respostaTime.toLowerCase() === "rosa") {
+      alert("Você é competente!");
+      continuarPerguntando = false; // Sai do loop após a resposta correta
+    } else {
+      alert("Você é imcopetente!");
+    }
   }
 }
